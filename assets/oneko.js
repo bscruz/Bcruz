240663e4b54a9bdd8907e7a0a7ed7fcf0babf707
bscruz <143441734+bscruz@users.noreply.github.com>
2025-01-25 17:57:13 -0800

    Changed oneko cat -> dog

--- assets/oneko.js
+++ assets/oneko.js
@@ -94,7 +94,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = 2147483647;
 
-    let nekoFile = "https://bscruz.github.io/Bcruz/img/onekko.gif"
+    let nekoFile = "https://bscruz.github.io/Bcruz/img/oneko-dog.gif"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
